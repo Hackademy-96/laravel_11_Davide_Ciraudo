19b992eed2ceb9909f2bc742ab85e3726265d9e2
Davide Ciraudo <davideciraudoecomm@icloud.com>
2023-10-26 15:45:26 +0200

    fine installazione pacchetti e librerie_ fine creazione dei components ed inizio progetto

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,120 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Collegati a noi tramite i nostri Social:</span>
+      </div>
+      <!-- Left -->
+  
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>SkyCreators®
+            </h6>
+            <p>
+              Creatori dell'aria! Ci occupiamo della creazione di siti web su tutto il territorio nazionale, tramite piattaforma shopify per chi volesse avere un sito web facile e poco costoso o tramite il linguaggio del codice creiamo siti web più complessi e dinamici.
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Prodotti
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Sito Shopify</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Sito Code Con Gestinale</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">eCommerce Shopify</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">eCommerce Code</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Legale
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Informativa sulla Privacy</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Termini e Condizioni del Servizio</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Refound Policy</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Informativa legale</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contatti</h6>
+            <p><i class="fas fa-home me-3"></i> Adrano (CT)</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              davideciraudoecomm@icloud.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> 339 681 82 50</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2021 Copyright:
+      <a class="text-reset fw-bold" href="https://SkyCreators.it">SkyCreators.it</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Altamura™</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto justify-content-center d-flex w-100 mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="">HomePage</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="">Tutti i Giochi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="">Crea Gioco</a>
+                </li>
+            </ul>  
+            @auth
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item">
+                            <form method="POST" action="">
+                                @csrf
+                                <button class="btn " type="submit">Logout</button>
+                            </form>
+                        </li> 
+                        <li class="dropdown-item">
+                            <a class="nav-link active" href="">I Miei Giochi</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>         
+            
+            @endauth
+            @guest
+            <ul class=" navbar-nav">
+               <li class="nav-item">
+                <a class="nav-link active" href="">Registrati</a>                           
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="">Accedi</a>
+            </li>   
+            </ul>
+              
+            @endguest
+            
+        </div>
+    </div>
+</nav>
